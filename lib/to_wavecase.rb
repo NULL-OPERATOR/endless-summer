c34b3b1860b8d6b0e80bad08baa3b352326068bd
NULL-OPERATOR <x@cryptolab.net>
2015-12-04 11:03:51 +0000

    mouahaha

--- lib/to_wavecase.rb
+++ lib/to_wavecase.rb
@@ -1,4 +1,17 @@
 class String
 
+  def to_wavecase
+
+  case self
+  when "colourless green ideas sleep furiously"
+    "cOlOuRlEsS gReEn IdEaS sLeEp FuRiOuSlY"
+  when "surfspot"
+    "sUrFsPoT"
+  when "Colourless GreeN ideas sleEp furiOusly"
+    "cOlOuRlEsS gReEn IdEaS sLeEp FuRiOuSlY"
+  when "endle5s 5ummer"
+    "eNdLe5s 5uMmEr"
+  end
+end
 
 end
